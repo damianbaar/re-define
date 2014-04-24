f935df6c55d0bc3c7f7f43ff5374b110e43adb69
Damian Baar <damian.baar@gmail.com>
2014-04-24 19:19:12 +0200

    updated readme, some minor changes

--- README.md
+++ README.md
@@ -1,15 +1,73 @@
-## reason
+## re-define
+Convert anything to anything.
 
 ### Getting Started
 Install the module: `npm install -g re-define`
 
 ###Usage
 ```
-re-define
+Usage: re-define [options]
+
+  Options:
+
+    -h, --help                  output usage information
+    -v, --verbose               Verbose mode
+    -c, --config [name]         Re-define config
+    -i, --input [code/file]     Arbitrary code
+    -w, --wrapper [iife/empty]  Wrapper type
+    -b, --base [dir]            Base folder for project, i.e. .
+    -m, --main [file]           Main file
+    -o, --output [file]         Output
 ```
+
+#### Examples
+
+##### from stream
+`re-define -w empty -i "define('a',['jquery','underscore'], function($,_) { console.log($, _) })"`
+
+```
+//output:
+var a = function ($, _) {
+  console.log($, _);
+}(jquery, underscore);
+```
+
+`re-define -w iife -i "define('a',['jquery','underscore'], function($,_) { console.log($, _) })" `
+```
+(function(jquery,underscore){var a = function ($, _) {
+  console.log($, _);
+}(jquery, underscore);
+})(jquery,underscore)
+```
+
+##### from file
+`re-define -m example/demo/main.js -o test.js -b . -v`
+or
+`re-define -c build.conf`
 
 ###Config
 ```
-
-###Result
+{
+  base: '.'
+, main: ''
+, out: ''
+, namespace: 'ns'
+, wrapper: 'empty'
+, converters: {
+  'amd-define':  {
+    resolver: require('./converter/amd-define-resolver')
+  , transformer: require('./converter/amd-define-transformer')
+  }
+  , 'amd-require': {
+    resolver: require('./converter/amd-require-resolver')
+  , transformer: require('./converter/amd-require-transformer')
+  }
+}
+, wrappers: {
+  'empty': require('./wrapper/empty')
+, 'iife': require('./wrapper/iife')
+, 'amd-define': require('./wrapper/amd-define')
+}
+, formatter: { format: {indent: {style: '  ', base: 0}, space: ' '}}
+}
 ```
--- bin/re-define.js
+++ bin/re-define.js
@@ -7,28 +7,54 @@ var program = require('commander')
   , read = function(path) { return fs.readFileSync(path, 'utf-8') }
   , config = require('../lib/config')
   , resolve = require('path').resolve
+  , colors = require('colors')
 
   program
     .option('-v, --verbose', 'Verbose mode')
-    .option('-c, --config [name]', 'Config')
-    .option('-i, --input [code/file]', 'Input')
-    .option('-w, --wrapper [iife/empty]', 'Input')
+    .option('-c, --config [name]', 'Re-define config')
+    .option('-i, --input [code/file]', 'Arbitrary code')
+    .option('-w, --wrapper [iife/empty]', 'Wrapper type')
+    .option('-b, --base [dir]', 'Base folder for project')
+    .option('-m, --main [file]', 'Main file')
+    .option('-o, --output [file]', 'Output')
     .parse(process.argv)
 
   var input = program.input && resolveInput(program.input)
     , userConfig = program.config && resolveConfig(program.config) || {}
-    , done = function(result) {
-      console.log('\n----- RESULT ------\n', result, '\n------------')
-    }
 
   if(program.wrapper) userConfig.wrapper = program.wrapper
+  if(program.base)    userConfig.base = program.base
+  if(program.main)    userConfig.main = program.main
+  if(program.output)  userConfig.output = program.output
 
-  if(userConfig && !input) redefine.convert.files(config(userConfig), done)
-  if(input) redefine.convert.stream(config(userConfig), input, done)
+    console.log('otuput', program.output, userConfig.output)
+
+  if(input) {
+    redefine.stream(config(userConfig), input, done)
+  }
+  else if(userConfig.base && userConfig.main) {
+    redefine.files(config(userConfig), done)
+  }
+
+  function done(result) {
+    if(program.verbose) {
+      console.log('\n----- RESULT ------\n'.green)
+      console.log('DETAILS\n'.grey)
+      console.log(result.details)
+      console.log('\nCODE\n'.grey)
+      console.log(result.code)
+      console.log('\n------------'.green)
+    }
+
+    if(program.output) {
+      fs.writeFileSync(resolve(program.output), result.code)
+    }
+  }
 
   function resolveConfig(config) {
-    return isFile(config) ? JSON.parse(read(resolve(config)))
-                          : {}
+    return isFile(config)
+            ? JSON.parse(read(resolve(config)))
+            : {}
   }
 
   function resolveInput(input) {
