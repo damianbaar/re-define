f4bed4677d09d81d3161ec4d66b91a77ce05781f
Damian Baar <damian.baar@gmail.com>
2014-04-25 23:33:26 +0200

    R+W stream

--- bin/re-define.js
+++ bin/re-define.js
@@ -5,67 +5,37 @@ var program = require('commander')
   , resolve = fs.resolve
   , read = function(path) { return fs.readFileSync(path, 'utf-8') }
   , resolve = require('path').resolve
-  , _ = require('underscore')
-  , colors = require('colors')
   , config = require('../lib/config')
   , redefine = require('../lib/index')
 
   program
     .option('-v, --verbose', 'Verbose mode')
     .option('-c, --config [name]', 'Re-define config')
-    .option('-i, --input [code/file]', 'Arbitrary code')
     .option('-w, --wrapper [iife/empty]', 'Wrapper type')
     .option('-b, --base [dir]', 'Base folder for project')
     .option('-m, --main [file]', 'Main file')
     .option('-o, --output [file]', 'Output')
+    .option('-f, --follow', 'Whether should resolve whole dep tree')
     .parse(process.argv)
 
-  var input = program.input && resolveInput(program.input)
-    , userConfig = program.config && resolveConfig(program.config) || {}
+  var userConfig = program.config && JSON.parse(read(resolve(program.config))) || {}
 
   if(program.wrapper) userConfig.wrapper = program.wrapper
   if(program.base)    userConfig.base = program.base
   if(program.main)    userConfig.main = program.main
   if(program.output)  userConfig.output = program.output
-  if(program.verbose)  userConfig.verbose = program.verbose
+  if(program.verbose) userConfig.verbose = program.verbose
+  if(program.follow)  userConfig.follow = program.follow
 
   userConfig = config(userConfig)
 
-  if(input) redefine.stream(userConfig, input, done)
-  if(userConfig.base && userConfig.main) redefine.files(userConfig, done)
+  process.stdin
+         .pipe(redefine.stream(userConfig))
+         .pipe(process.stdout)
 
-  function done(result) {
-    if(userConfig.verbose) {
-      var details = _.map(result.details, function(d) { return _.omit(d, "converter", "content") })
-      console.log('\n----- RESULT ------\n'.green)
-      console.log('DETAILS\n'.grey)
-      console.log(details)
-      console.log('\nCODE\n'.grey)
-      console.log(result.code)
-      console.log('\n------------'.green)
-    }
-
-    if(userConfig.output) {
-      fs.writeFileSync(resolve(userConfig.output), result.code)
-    }
-  }
-
-  function resolveConfig(config) {
-    return isFile(config)
-            ? JSON.parse(read(resolve(config)))
-            : {}
-  }
-
-  function resolveInput(input) {
-    var path = resolve(input)
-    return isFile(input) ? read(path) : input
-  }
-
-  function isFile(path) {
-    var stats = fs.lstatSync
-      , exists = fs.existsSync
-
-    return path
-           && exists(path)
-           && stats(path).isFile()
-  }
+  if(userConfig.base && userConfig.main)
+    redefine.files(userConfig, function(result) {
+      userConfig.output && fs.writeFileSync(resolve(userConfig.output), result)
+      console.log(result)
+      process.exit()
+    })
