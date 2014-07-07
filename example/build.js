f0d8410cdf04862cd56505d570bcfa7227b6c42f
Damian Baar <damian.baar@gmail.com>
2014-07-08 00:25:19 +0200

    getting deps from already resolved umd bundles

--- example/build.js
+++ example/build.js
@@ -1,109 +1,88 @@
-(function(parent, factory) {
+(function (parent, factory){
   if (typeof define === 'function' && define.amd) {
     define('module_name', ['async'], factory)
   } else if (typeof exports === 'object') {
-    module.exports = factory(require('async'))
+  module.exports = factory(require('async'))
   } else {
     var async = parent.async
-
+    
     parent['module_name'] = factory(async)
   }
-}(this, function(async) {
+  }(this, function (async) {
   this.async = async
 
-
-
+  
+   
   this['jquery'] = (function() {
     var __context = this
     var _scope_ = {}
-    _scope_['lib-2/dep1'] = (function() {
-      return {
-        name: 'dep'
-      };
-    })();
-    _scope_['lib/dep1'] = (function() {
-      var a = require('lib-2/dep1');
-      return {
-        name: 'dep'
-      };
-    })();
-    _scope_['jquery'] = (function() {
-      var a = require('lib/dep1');
-      var a = 'jquery';
-      return {
-        name: a
-      };
-    })();
+    _scope_['lib-2/dep1'] = (function() { return { name: 'dep' }; })();
+_scope_['lib/dep1'] = (function() { var a = require('lib-2/dep1');
+return { name: 'dep' }; })();
+_scope_['jquery'] = (function() { var a = require('lib/dep1');
+var a = 'jquery';
+return { name: a }; })();
 
     return _scope_['jquery']
 
-    function require(name) {
-      return __context[name] || _scope_[name]
-    }
-  })
-  .call(this);
-
-  (function(parent) {
-    return parent.d3 = {
-      name: 'd3'
-    }
-  })(this)
-  ;
-
-
+    function require (name) { return __context[name] || _scope_[name] }
+    })
+    .call(this);
+   
+   (function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['b'], factory)
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('b'))
+  } else {
+    root.returnExports = factory(root.b)
+  }
+}(this, function (b) {
+  return parent.d3 = {d3: 'd3'}
+}))
+;
+  
+   
   this['main'] = (function() {
     var __context = this
     var _scope_ = {}
-    _scope_['model/helper'] = (function() {
-      var async = require('async');
-      return {
-        getAsync: async
-      };
-    })();
-    _scope_['model/model'] = (function() {
-      var helper = require('model/helper');
-      var async = helper.getAsync();
-      return {
-        getData: function() {
-          return async ? 'async_data' : 'data';
-        }
-      };
-    })();
-    _scope_['text!./view/template.html'] = '<li></li><li></li><li></li><li></li>';
-    _scope_['view/view'] = (function() {
-      var model = require('model/model'),
-        tmpl = require('text!./view/template.html');
-      return function() {
-        return {
-          model: model.getData(),
-          view: tmpl
-        };
-      };
-    })();
-    _scope_['text!./template.html'] = '<div id="module_name">test</div>';
-    _scope_['main'] = (function() {
-      var $ = require('jquery');
-      var d3 = require('d3');
-      var model = require('model/model');
-      var view = require('view/view');
-      var template = require('text!./template.html');
-      return {
-        jquery: $,
-        d3: d3,
-        model: model,
-        view: view,
-        template: template
-      };
-    })();
+    _scope_['model/helper'] = (function() { var async = require('async');
+return { getAsync: async }; })();
+_scope_['model/model'] = (function() { var helper = require('model/helper');
+var async = helper.getAsync();
+return {
+    getData: function () {
+        return async ? 'async_data' : 'data';
+    }
+}; })();
+_scope_['text!./view/template.html'] = '<li></li><li></li><li></li><li></li>';
+_scope_['view/view'] = (function() { var model = require('model/model'), tmpl = require('text!./view/template.html');
+return function () {
+    return {
+        model: model.getData(),
+        view: tmpl
+    };
+}; })();
+_scope_['text!./template.html'] = '<div id="module_name">test</div>';
+_scope_['main'] = (function() { var $ = require('jquery');
+var d3 = require('d3');
+var model = require('model/model');
+var view = require('view/view');
+var template = require('text!./template.html');
+return {
+    jquery: $,
+    d3: d3,
+    model: model,
+    view: view,
+    template: template
+}; })();
 
     return _scope_['main']
 
-    function require(name) {
-      return __context[name] || _scope_[name]
-    }
-  })
-  .call(this);
+    function require (name) { return __context[name] || _scope_[name] }
+    })
+    .call(this);
+   
 
-
-  return this.main
-}.bind({})))
+   return this.main 
+  }.bind({})))
