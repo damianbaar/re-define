fa1ad105c743c4bdfedff60ed095c9602581de41
Damian Baar <damian.baar@gmail.com>
2015-03-04 20:01:40 +0100

    removing leftovers

--- examples/umd/bundle.js
+++ examples/umd/bundle.js
@@ -1,4 +1,4 @@
-//re-define version:0.0.3-alpha
+//re-define version:1.14.0
 //externals: dep1,dep2,async/async,lodash/dep1
 ;(function (parent, factory){
   if (typeof define === 'function' && define.amd) {
